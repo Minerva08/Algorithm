afca9c3c9bcf0c5393fc3cb34e89a88dc477c16e
Jaee <elavl@naver.com>
2024-10-19 09:42:01 +0900

    Title: 순서쌍 만들기, Time: 107ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241019/순서쌍 만들기/making-order-pair.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int cnt = Integer.parseInt(st.nextToken());
+        for(int i=cnt; i>0; i--){
+            for(int j=cnt; j>0; j--){
+                System.out.printf("("+String.valueOf(i)+","+String.valueOf(j)+") ");
+            }
+            System.out.println();
+        }
+    }
+}
