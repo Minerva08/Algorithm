2fcc57ee3ef04736fd7eb5a8e052353185e695e9
Jaee <elavl@naver.com>
2024-10-25 19:02:07 +0900

    Title: 배열의 특정 위치 찾기, Time: 94ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241025/배열의 특정 위치 찾기/find-specific-location-fo-array.java
+++ 241025/배열의 특정 위치 찾기/find-specific-location-fo-array.java
@@ -23,7 +23,7 @@ public class Main {
             }
         }
 
-        System.out.print(sum+" "+Math.floor((avg/3)*10)/10);
+        System.out.print(sum+" "+Math.round((avg/3)*100)/100.0);
     
     }
 }
